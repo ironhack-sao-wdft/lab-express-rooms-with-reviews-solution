f604170d13549d623a4e694ccf30f10df36b5ce3
Fernanda Rosa <ferosa91@gmail.com>
2021-03-01 22:32:51 +0100

    get iteration

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -3,16 +3,25 @@ const router = express.Router();
 
 const Room = require("../models/Room.model");
 
-router.post("/room", async (req,res) => {
-    try {        
-        const newRoom = await Room.create(req.body);
-        console.log(newRoom);
-        
-        return res.status(201).json(newRoom);
+router.post("/room", async (req, res) => {
+  try {
+    const newRoom = await Room.create(req.body);
+    console.log(newRoom);
 
-    } catch(err) {
-        return res.status(500).json({ msg: err});
-    }
+    return res.status(201).json(newRoom);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+});
+
+router.get("/room", async (req, res) => {
+  try {
+    const rooms = await Room.find();
+
+    return res.status(200).json(rooms);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
 });
 
 module.exports = router;

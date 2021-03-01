11e025af0a4fd7d3a50a70a558ef46b54ceb7502
MCiappina <mciappops@gmail.com>
2021-03-01 18:43:26 -0300

    update iteration

--- server/routes/room.routes.js
+++ server/routes/room.routes.js
@@ -24,4 +24,37 @@ router.get("/room", async (req, res) => {
   }
 });
 
+router.get("/room/:id", async (req, res) => {
+  try {
+    const room = await Room.findOne({ _id: req.params.id });
+    console.log(room);
+
+    if (!room) {
+      return res.status(404).json({ msg: "Room not found" });
+    }
+    return res.status(200).json(room);
+  } catch (err) {
+    return res.status(500).json({ msg: err });
+  }
+});
+
+router.patch("/room/:id", async (req,res) => {
+    try {
+        const updatedRoom = await Room.findOneAndUpdate(
+            { _id: req.params.id },
+            { $set: req.body },
+            { new: true }
+        );
+
+        if (!updatedRoom) {
+            return res.status(404).json({ msg: "Pet not found" });
+        }
+
+        return res.status(200).json(updatedRoom);
+
+    } catch (err) {
+        return res.status(500).json({ msg: err });
+    }
+})
+
 module.exports = router;
